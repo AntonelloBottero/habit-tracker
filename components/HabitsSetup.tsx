3e2cbc886c084dfbc3858e612fd3f6e1d096a3a1
AntonelloBottero <antonello.bottero@gmail,com>
2025-09-21 20:47:29 +0200

    feat: habits setup

--- components/HabitsSetup.tsx
+++ components/HabitsSetup.tsx
@@ -1,3 +1,10 @@
+// Card made of 2 sections -> Good and Bad habits
+// every section accepts any number of items
+// adding or editing an element opens a dedicated form modal
+// Form modal has default and advanced settings:
+// - Name
+// - When (every day, selected days, except weekends, n days per week/month, etc)
+// - Icon
 export default function HabitsSetup() {
     return (
         <div className="block w-lg p-6 bg-white border border-gray-200 rounded-lg shadow-ht hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
