b238a94b55305179ea55327dc12d9d5763795f9f
AntonelloBottero <antonello.bottero@gmail,com>
2025-09-29 21:05:04 +0200

    feat: color picker

---
+++ components/ColorPicker.tsx
@@ -0,0 +1,9 @@
+/**
+ * 1. manage menu with FloatingMenu
+ * 2. Manage default colors with a predefined set with the custom colors picked by the user
+ * 3. Export util to save in the options the custom colors user picks
+ */
+
+export default function ColorPicker() {
+    return null
+}
