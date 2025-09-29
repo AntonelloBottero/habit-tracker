c1fa9304b27242bfa85dc25d247dcda74ceed5cf
AntonelloBottero <antonello.bottero@gmail,com>
2025-09-29 20:45:11 +0200

    fix: components

--- components/InputWrapper.tsx
+++ components/InputWrapper.tsx
@@ -18,7 +18,7 @@ export default function InputWrapper({ errorMessages = [], label = '', input}: P
         <>
             {label && <label className={'block mb-1 text-sm font-medium'+errorMessages?.length ? 'text-red-700 dark:text-red-500' : ''}>{label}</label>}
             {cloneElement(input, {
-                className: (input.props.className || '') + (errorMessages?.length ? ' bg-red-50 outline-red-500 text-red-900 placeholder:text-red-300 focus:ring-red-500 focus:border-red-500 dark:text-red-500 dark:placeholder:text-red-500 dark:border-red-500' : '')
+                className: (input.props.className || '') + (errorMessages?.length ? ' bg-red-50 outline-red-500 text-red-900 placeholder:text-red-500 focus:ring-red-500 focus:border-red-500 dark:text-red-500 dark:placeholder:text-red-500 dark:border-red-500' : '')
             })}
             {errorMessages.length > 0 && (
                 <div className="mt-1 text-sm text-red-600 dark:text-red-500">
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -23,7 +23,7 @@ const Modal = forwardRef<ModalRef>(({ title, children }: Props, ref) => {
     }))
 
     const modal = (
-        <div tabIndex={-1} aria-hidden="true" className={(!value ? 'hidden ' : '') + 'overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-white/95'}>
+        <div tabIndex={-1} aria-hidden="true" className={(!value ? 'hidden ' : 'flex') + ' overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-white/95'}>
             <div className="relative p-4 w-full max-w-2xl max-h-full">
                 <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
                     <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
