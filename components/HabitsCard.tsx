23b88c8a7d036e6896d876cc75800b9b4757d11a
AntonelloBottero <antonello.bottero@gmail,com>
2025-12-27 18:29:28 +0100

    refactor: icons

--- components/HabitsCard.tsx
+++ components/HabitsCard.tsx
@@ -1,4 +1,5 @@
 import { HabitsSchema } from "@/db/DbClass"
+import { ArrowOutward } from "@project-lary/react-material-symbols-700-rounded"
 
 interface Props {
     habit: HabitsSchema & { id: number }
@@ -7,6 +8,7 @@ interface Props {
 export default function HabitsCard({ habit }: Props) {
   return habit ? (
     <div className="ht-interaction p-6 bg-white shadow-ht">
+      <ArrowOutward className="text-xl" />
       {habit.name}
     </div>
   ) : null
