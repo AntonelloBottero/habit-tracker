f7a481cf54648c0f4f380d7cd8f4a33c3cdf87da
AntonelloBottero <antonello.bottero@gmail,com>
2026-01-02 20:43:54 +0100

    refactor: HabitsCard

--- components/HabitsCard.tsx
+++ components/HabitsCard.tsx
@@ -1,5 +1,5 @@
 import { HabitsSchema } from "@/db/DbClass"
-import { ArrowOutward, CalendarToday, CheckCircle } from "@project-lary/react-material-symbols-700-rounded"
+import { NorthEast, SouthEast, CalendarToday, CheckCircle } from "@project-lary/react-material-symbols-700-rounded"
 import TonalIcon from '@/components/TonalIcon'
 
 interface Props {
@@ -18,17 +18,19 @@ export default function HabitsCard({ habit, className = '', onClick }: Props) {
   return habit ? (
     <div className={`${className} ht-interaction py-3 px-4 rounded-lg bg-green-50 outline-1 outline-offset-1 outline-green-100 flex flex-col gap-2`} onClick={() => { handleOnClick()}}>
       <div className="flex items-center gap-2">
-        <TonalIcon color={habit.color} icon={<ArrowOutward className="text-xl" />} />
-        <div>
+        <div className="grow mr-2">
           {habit.name}
         </div>
+        <TonalIcon color={habit.color} icon={(
+          habit.type !== 'bad' ? <NorthEast className="text-xl" /> : <SouthEast className="text-xl" />
+        )} />
       </div>
       <div className="flex items-center flex-wrap gap-3">
         <div className="flex items-center gap-1 text-sm">
           <CalendarToday />
           <span>
             {habit.granularity}
-            {habit.granularity_times > 1 && `(${habit.granularity_times})`}
+            {habit.granularity_times > 1 && ` (${habit.granularity_times} times)`}
           </span>
         </div>
         {habit.enough_amount && (
