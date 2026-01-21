61d1ba309f774d98dbe871ae09db02ae84b8e544
AntonelloBottero <antonello.bottero@gmail,com>
2026-01-21 21:44:54 +0100

    feat: CardsInput

--- components/CardsInput.tsx
+++ components/CardsInput.tsx
@@ -1,15 +1,22 @@
+import { ReactNode } from "react"
+
 interface Props<T> {
+  value: never,
 	items: T[]
 	itemValue?: keyof T
 	itemTitle?: keyof T
+  content?: (item: T, selected: boolean) => ReactNode
 }
 
-export default function CardsInput<T extends Record<string, never>>({ items, itemValue = 'id', itemTitle = 'name'}: Props<T>) {
+export default function CardsInput<T extends Record<string, never>>({ value, items, itemValue = 'id', itemTitle = 'name', content}: Props<T>) {
+
   return (
     <div className="grid grid-cols-1 gap-3">
       {items.map(item => (
-        <div key={item[itemValue]} className="ht-interaction pt-2 pb-2 px-4 rounded-lg bg-white outline-1 outline-offset-1 outline-green-200 flex flex-col gap-2">
-          {item[itemTitle]}
+        <div key={item[itemValue]} className={`ht-interaction pt-2 pb-2 px-4 rounded-lg ${value === item[itemValue] ? 'bg-green-50 outline-green-200' : 'bg-white outline-stone-200' } outline-1 outline-offset-1 flex flex-col gap-2`}>
+          {!content
+            ? item[itemTitle]
+            : content(item, value === item[itemValue])}
         </div>
       ))}
     </div>
