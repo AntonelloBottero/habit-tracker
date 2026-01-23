854cd8e9e5aa5b1775bc192afc67c87a7d9ff8df
AntonelloBottero <antonello.bottero@gmail,com>
2026-01-23 17:47:30 +0100

    test: CardsInput

--- components/CardsInput.tsx
+++ components/CardsInput.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, ChangeEvent } from "react"
 
 interface Props<T> {
-  value: never
+  value: unknown
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void
 	items: T[]
 	itemValue?: string
@@ -21,10 +21,11 @@ export default function CardsInput<T extends object>({ value, onChange, items, i
   }
 
   return (
-    <div className="grid grid-cols-1 gap-3">
+    <div className="grid grid-cols-1 gap-3" role="cards-input">
       {items.map(item => (
         <div
           key={item[itemValue]}
+          role="cards-input-item"
           className={`ht-interaction pt-2 pb-2 px-4 rounded-lg ${value === item[itemValue] ? 'bg-green-50 outline-green-200' : 'bg-white outline-stone-200' } outline-1 outline-offset-1 flex flex-col gap-2`}
           onClick={() => { handleItemClick(item) }}
         >
--- tests/form-components.test.tsx
+++ tests/form-components.test.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react'
-import {render, screen, within, act, waitFor} from '@testing-library/react'
+import {render, screen, within, act, waitFor, fireEvent} from '@testing-library/react'
 import '@testing-library/jest-dom' // for toBeInTheDocument() assertion
 import { DbProvider } from '@/db/useDb'
 
@@ -38,6 +38,7 @@ describe('InputWrapper component', () => {
 // --- ColorPicker ---
 import ColorPicker from '@/components/ColorPicker'
 import {defaultColors} from '@/utils/constants'
+import CardsInput from '@/components/CardsInput'
 let colorValue: string = ''
 describe('ColorPicker component', () => {
   beforeEach(() => { // before each test the value gets reset
@@ -100,5 +101,91 @@ describe('ColorPicker component', () => {
     const PickerContext = within(PickerElement)
     const InputElement = PickerContext.getByDisplayValue(defaultColors[1])
     expect(InputElement).toBeInTheDocument()
+  })
+})
+
+// --- CardsInput ---
+const cardsInputItems = [
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3'}
+]
+
+describe('CardsInput', () => {
+  test('Display items', async () => {
+    await act(() => {
+      render(<CardsInput value="" items={cardsInputItems} />)
+    })
+
+    const CardsInputElement = screen.getByRole('cards-input')
+    const CardsInputContext = within(CardsInputElement)
+    const CardsInputItems = CardsInputContext.getAllByRole('cards-input-item')
+    await waitFor(() => {
+      expect(CardsInputItems.length).toBe(3)
+    })
+
+    const CardsInputItemContext = within(CardsInputItems[1])
+    const CardsInputItemLabel = CardsInputItemContext.findByLabelText(cardsInputItems[1].name)
+    await waitFor(() => {
+      expect(CardsInputItemLabel).toBeDefined()
+    })
+  })
+
+  test('Override content', async () => {
+    await act(() => {
+      render(<CardsInput value="" items={cardsInputItems} content={(item) => <h1>{item.name}</h1>} />)
+    })
+
+    const CardsInputElement = screen.getByRole('cards-input')
+    const CardsInputContext = within(CardsInputElement)
+    const CardsInputItems = CardsInputContext.getAllByRole('cards-input-item')
+    await waitFor(() => {
+      expect(CardsInputItems.length).toBe(3)
+    })
+
+    const CardsInputItemContext = within(CardsInputItems[1])
+    const CardsInputItemH1 = CardsInputItemContext.getByRole('heading', {level: 1})
+    const CardsInputItemLabel = CardsInputItemContext.findByLabelText(cardsInputItems[1].name)
+    await waitFor(() => {
+      expect(CardsInputItemH1).toBeDefined()
+      expect(CardsInputItemLabel).toBeDefined()
+    })
+  })
+
+  test('Value highlights item', async () => {
+    await act(() => {
+      render(<CardsInput value={2} items={cardsInputItems} />)
+    })
+    const CardsInputElement = screen.getByRole('cards-input')
+    const CardsInputContext = within(CardsInputElement)
+    const CardsInputItems = CardsInputContext.getAllByRole('cards-input-item')
+    await waitFor(() => {
+      expect(CardsInputItems.length).toBe(3)
+      expect(CardsInputItems[1]).toHaveClass('bg-green-50')
+      expect(CardsInputItems[1]).toHaveClass('outline-green-200')
+    })
+  })
+
+  test('Value changes after item click', async () => {
+    function setValue(value: string) {
+      expect(value).toBe(cardsInputItems[1].id)
+    }
+    await act(() => {
+      render(<CardsInput value={1} items={cardsInputItems} onChange={(e) => setValue(e.target.value)} />)
+    })
+    const CardsInputElement = screen.getByRole('cards-input')
+    const CardsInputContext = within(CardsInputElement)
+    const CardsInputItems = CardsInputContext.getAllByRole('cards-input-item')
+    await waitFor(() => {
+      expect(CardsInputItems.length).toBe(3)
+    })
+
+    fireEvent(
+      CardsInputItems[1],
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      }),
+    )
   })
 })
