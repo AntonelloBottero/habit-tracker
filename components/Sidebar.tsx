11d420ffe1f26ea128de1ef0b12f16934a6f951a
AntonelloBottero <antonello.bottero@gmail,com>
2026-02-23 10:51:25 +0100

    refactor: Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -15,33 +15,46 @@ interface Props {
 export default function Sidebar({ value, onChange, breakpoint = 'md', width = '256px', align = 'left', bordered = true, title, children }: Props) {
   const { breakpoints } = useBreakpoints()
 
+  const hasBreakpoint = useMemo(() => {
+    return breakpoints ? breakpoints[breakpoint] : false
+  }, [breakpoint, breakpoints])
+
   useEffect(() => {
     if(onChange) {
       onChange({
         target: {
-          value: breakpoints ? breakpoints[breakpoint] : false
+          value: hasBreakpoint
         }
       } as unknown as ChangeEvent<HTMLInputElement>)
     }
-  }, [breakpoint, breakpoints])
+  }, [hasBreakpoint])
 
-  const className = useMemo(() => {
+  const asideClassName = useMemo(() => {
+    return [
+      { class: 'fixed w-full h-full flex z-10', value: !hasBreakpoint },
+      { class: 'justify-end', value: !hasBreakpoint && align === 'right' }
+    ].filter(cn => cn.value).map(cn => cn.class).join(' ')
+  }, [hasBreakpoint, align])
+
+  const contentClassName = useMemo(() => {
     return [
       { class: 'border-r-1', value: bordered && align === 'left' },
-      { class: 'border-l-1', value: bordered && align === 'right' },
+      { class: 'border-l-1 ml-auto', value: bordered && align === 'right' },
       { class: 'border-stone-200', value: bordered }
     ].filter(cn => cn.value).map(cn => cn.class).join(' ')
   }, [bordered, align])
 
   return value ? (
-    <aside className={`${className} gradient-primary h-full min-h-[100vh] flex-shrink self-stretch overflow-y-auto z-10`} style={{ maxWidth: width, flexBasis: width }}>
-      <div className="py-4 px-4 flex flex-col gap-4">
-        {title && (
-          <div className="text-xl font-monda font-bold my-2">
-            {title}
-          </div>
-        )}
-        {children}
+    <aside className={`${asideClassName} max-h-[100vh]`}>
+      <div className={`${contentClassName} bg-white max-w-full h-full overflow-y-auto`} style={{ width }}>
+        <div className="py-4 px-4 flex flex-col gap-4">
+          {title && (
+            <div className="text-xl font-monda font-bold my-2">
+              {title}
+            </div>
+          )}
+          {children}
+        </div>
       </div>
     </aside>
   ) : null
