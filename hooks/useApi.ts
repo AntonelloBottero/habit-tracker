55533f4c41485032b8f2cd5584f4e15a1d12c528
AntonelloBottero <antonello.bottero@gmail,com>
2026-03-13 21:50:28 +0100

    feat: useApi

--- hooks/useApi.ts
+++ hooks/useApi.ts
@@ -1,3 +1,12 @@
+/**
+ * l'esigenza di rifattorizzazione nasce dal fatto che, per avere un db crud che gestisca delle risorse correttamente tipizzate, bisogna passare come parametro a useDbCrud il model della risorsa
+ * - ogni risorsa ha il suo hook api
+ * - qui sono messe a disposizione tutte le api possibili relative ad una determinata risorsa
+ *   - oppure semplicemente è una facade di useDbCrud
+ * - ma le risorse devono poter comunicare, quindi bisogna in qualche modo fare in modo i relativi hooks comunichino
+ * - ogni crud api fa riferimento a useDbCrud per le varie operazioni
+ */
+
 import { DbResourceSchema } from "@/db/DbClass"
 
 type Query = Record<string, never>
