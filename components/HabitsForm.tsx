9d57fd8139e5c1af427e3a24e00c605ec31947b6
AntonelloBottero <antonello.bottero@gmail,com>
2026-04-17 21:21:42 +0200

    fix: HabitsForm

--- components/HabitsForm.tsx
+++ components/HabitsForm.tsx
@@ -32,7 +32,6 @@ const rules: Rules = {
 const granularities: string[] = ['daily', 'weekly', 'monthly', 'yearly']
 
 export default function FormHabits({ values, onSave, onDelete }: Props) {
-  console.log('FormHabits render')
   const { options } = useDb()
   const setupDone = (options.current.last_setup_at || '') > DateTime.now().toISO()
 
