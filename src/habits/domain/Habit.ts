1d120fb901af0e27227cad8cf1be73331b1d5ef3
AntonelloBottero <antonello.bottero@gmail,com>
2026-08-31 21:48:51 +0200

    feat: Habit entity

--- src/habits/domain/Habit.ts
+++ src/habits/domain/Habit.ts
@@ -1,59 +1,37 @@
-export type Type = 'good' | 'bad'
-export type Granularity = 'daily' | 'weekly' | 'monthly' | 'yearly'
+const types = ['good', 'bad'] as const
+const granularities = ['daily', 'weekly', 'monthly', 'yearly'] as const
+
+export type Type = typeof types[number]
+export type Granularity = typeof granularities[number]
+
+export interface HabitProps {
+    type: Type,
+    name: string,
+    color: string,
+    granularity: Granularity,
+    includeWeekends: boolean,
+    granularityTimes: number,
+    enoughAmount: string,
+    manageFrom: Date
+}
 
 export class Habit {
-    private _type: Type
-    private _name: string
-    private _color: string
-    private _granularity: Granularity
-    private _includeWeekends: boolean
-    private _granularityTimes: number
-    private _enoughAmount: string
-    private _manageFrom: Date
+    private _props: HabitProps
 
-    constructor(
-        type: Type,
-        name: string,
-        color: string,
-        granularity: Granularity,
-        includeWeekends: boolean,
-        granularityTimes: number,
-        enoughAmount: string,
-        manageFrom: Date
-    ) {
-        this._type = type
-        this._name = name
-        this._color = color
-        this._granularity = granularity
-        this._includeWeekends = includeWeekends
-        this._granularityTimes = granularityTimes
-        this._enoughAmount = enoughAmount
-        this._manageFrom = manageFrom
+    constructor(props: HabitProps) {
+        if(!types.includes(props.type)) {
+            throw new Error('Type is neither good nor bad')
+        }
+        if(!props.name) {
+            throw new Error('Name not set')
+        }
+        if(!granularities.includes(props.granularity)) {
+            throw new Error('Granularity not supported')
+        }
+        this._props = props
     }
 
-    // Getters
-    get type(): Type {
-        return this._type
-    }
-    get name(): string {
-        return this._name
-    }
-    get color(): string {
-        return this._color
-    }
-    get granularity(): Granularity {
-        return this._granularity
-    }
-    get includeWeekends(): boolean {
-        return this._includeWeekends
-    }
-    get granularityTimes(): number {
-        return this._granularityTimes
-    }
-    get enoughAmount(): string {
-        return this._enoughAmount
-    }
-    get manageFrom(): Date {
-        return this._manageFrom
+    public toPrimitives(): Readonly<HabitProps> {
+        return Object.freeze({...this._props})
     }
 }
