c7237b6ceb5ee92bae8fbdd3ae7ae7a4d6c1f8ab
AntonelloBottero <antonello.bottero@gmail,com>
2026-09-03 21:39:03 +0200

    feat: CreateHabit use case

--- src/habits/use-cases/CreateHabit.ts
+++ src/habits/use-cases/CreateHabit.ts
@@ -1,3 +1,19 @@
+import { Habit, type HabitProps } from '../domain/Habit'
+import { type SaveGateway } from '../../shared/gateways/contracts'
+
+export type CreateHabitInputDTO = HabitProps & {}
+export type CreateHabitOutputDTO = HabitProps & {
+    createdAt: Date
+}
+
+export type CreateHabitGateway = SaveGateway<CreateHabitInputDTO> & {
+    findByName: (name: string) => Promise<CreateHabitOutputDTO | null>
+}
+
 export class CreateHabit {
-    
+    private _saveGateway: CreateHabitGateway
+
+    constructor(saveGateway: CreateHabitGateway) {
+        this._saveGateway = saveGateway
+    }
 }
--- src/shared/gateways/contracts.ts
+++ src/shared/gateways/contracts.ts
@@ -1,13 +1,16 @@
-export interface SearchGateway<T, ID> {
-    show: (id: ID) => Promise<T | null>
+export type ID = string | null
+
+export interface SearchGateway<T> {
+    show: (id: string) => Promise<T | null>
     index: () => Promise<T[]>
 }
 
 export interface SaveGateway<T> {
-    store: (data: T) => Promise<T>
-    update: (data: T) => Promise<T>
+    store: (data: T) => Promise<T & { createdAt: Date }>
+    update: (data: T) => Promise<T & { updatedAt: Date }>
+    generateId: () => Promise<ID>
 }
 
-export interface DeleteGateway<ID> {
+export interface DeleteGateway {
     delete: (id: ID) => Promise<void>
 }
